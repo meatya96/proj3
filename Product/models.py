bcae13c1160a630bc93fdf9dabfc3c24c9ab8393
Meatya <meatya96@gmail.com>
2024-06-09 17:19:15 +0500

    20.1

---
+++ Product/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наиминование категории")
+    description = models.TextField(verbose_name="Описание категории")
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наиминование продукта")
+    description = models.TextField(verbose_name="Описание продукта")
+    photo = models.ImageField(
+        upload_to="Product/photo", blank=True, null=True, verbose_name="Фото"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        null=True,
+        blank=True,
+        related_name="Product",
+    )
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    manufactured_at = models.CharField(
+        max_length=100, verbose_name="Дата производства продукта", blank=True, null=True
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "description"]
+
+    def __str__(self):
+        return self.name
