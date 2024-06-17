c9016bfb5e39f1f2f244033fc4bc332ac1a1f0fc
Meatya <meatya96@gmail.com>
2024-06-17 16:49:12 +0500

    20.2 правки

--- Product/urls.py
+++ Product/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from Product.apps import ProductConfig
 
-from Product.views import product_list
+from Product.views import product_list, single_product
+
 app_name = ProductConfig.name
 
 urlpatterns = [
-    path('', product_list)
+    path('', product_list, name='product_list'),
+    path('product/<int:pk>/', single_product, name='single_product')
 ]
--- Product/views.py
+++ Product/views.py
@@ -6,4 +6,10 @@ from Product.models import Product
 def product_list(request):
     products = Product.objects.all()
     context = {"products": products}
-    return render(request, 'product_list.html', context)
+    return render(request, 'product/product_list.html', context)
+
+
+def single_product(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {"product": product}
+    return render(request, 'product/single_product.html', context)
